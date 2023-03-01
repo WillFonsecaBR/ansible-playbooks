e24527758ff9bb6f00110dd1a10584cfdf03e0d6
Willian Fonseca <willian.alves2109@gmail.com>
2023-03-01 12:20:15 -0300

    Organizando projeto

--- SUPER-PLAYBOOK/roles/os-radhat/files/virtualbox-fedora-37.sh
+++
@@ -1,9 +0,0 @@
-cat <<EOF | sudo tee /etc/yum.repos.d/virtualbox.repo 
-[virtualbox]
-name=Fedora $releasever - $basearch - VirtualBox
-baseurl=http://download.virtualbox.org/virtualbox/rpm/fedora/36/\$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://www.virtualbox.org/download/oracle_vbox.asc
-EOF
